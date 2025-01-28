945914654af43c033e2933476bf27823b3d4cd83
martindholmes <mholmes@uvic.ca>
2025-01-28 15:11:10 -0800

    Initial work on issue #21.

--- js/bbti.js
+++ js/bbti.js
@@ -31,3 +31,73 @@
 
 "use strict";
 
+//This is the var which will be an instance of the checkboxAligner
+//class.
+var cbAligner;
+
+/** @class checkboxAligner
+  * @description This class creates an object whose job is 
+  *              to align the county and city checkbox collections,
+  *              such that whichever counties are checked, the only
+  *              city/town options visible are those within those 
+  *              counties.
+  * 
+  *              It can be instantiated on any page, which will cause
+  *              the JSON it needs to be downloaded and presumably cached,
+  *              but obviously it will do nothing unless the search 
+  *              controls are present on the page. 
+  */
+class checkboxAligner{
+    constructor(){
+      //Headers used for all AJAX fetch requests.
+      this.fetchHeaders = {
+        credentials: 'same-origin',
+        cache: 'default',
+        headers: {'Accept': 'application/json'},
+        method: 'GET',
+        redirect: 'follow',
+        referrer: 'no-referrer'
+      };
+      //Array mapping to be constructed from JSON.
+      this.citiesToCounties = null;
+      this.cityCheckboxes = document.querySelectorAll('fieldset[title *= "City"]>ul.ssDescCheckboxList input[type="checkbox"]');
+      this.countyCheckboxes = document.querySelectorAll('fieldset[title *= "County"]>ul.ssDescCheckboxList input[type="checkbox"]');
+      if (this.countyCheckboxes.length > 0){
+        this.getJson();
+      }
+
+    }
+    async getJson(){
+        let fch = await fetch('js/citiesToCounties.json', this.fetchHeaders)
+        .then(function(response){return response.json();})
+        .then(function(json){this.citiesToCounties = json}.bind(this));
+        this.setupCheckboxDependencies();
+    }
+    /**
+     * @function checkboxAligner~setupCheckboxDependencies
+     * @description This method assigns an onchange event to county selection
+     *              checkboxes, so that the city/town checkboxes available are
+     *              limited to those from the selected counties.
+     */
+    setupCheckboxDependencies(){
+        console.log('Setting up checkbox dependencies.')
+        this.countyCheckboxes.forEach(
+            cb => {cb.addEventListener('change', function(){this.alignCheckboxes(cb);}.bind(this))});
+    }
+    /**
+     * @function checkboxAligner~alignCheckboxes
+     * @description This method performs the alignment by retrieving the 
+     *              list of selected counties, then checking each city 
+     *              and either showing it or unchecking and hiding it.
+     * @param {HTMLInputElement} sender The checkbox which has triggered
+     *                            the function.
+     */
+    alignCheckboxes(sender){
+        console.log(sender.getAttribute('value'));
+    }
+}
+
+/**
+ * Instantiate on load.
+ */
+window.addEventListener('load', function(){cbAligner = new checkboxAligner();});
