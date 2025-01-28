6d042eec4051b5d2c36b023ee2c8190336070f37
martindholmes <mholmes@uvic.ca>
2025-01-28 14:13:27 -0800

    Preparing for some JS enhancement.

---
+++ js/bbti.js
@@ -0,0 +1,33 @@
+/*             bbti.js                */
+/*        Author: Martin Holmes.      */
+/*        University of Victoria.     */
+
+/** This file is part of the projectEndings BBTI
+  * project.
+  *
+  * Free to anyone for any purpose, but
+  * acknowledgement would be appreciated.
+  * 
+  * This file is provided as an example of how a project
+  * might implement support for highlighting search hits
+  * in a target page by including some simple JS in all
+  * pages on the site. It can most likely be used as-is
+  * in your project, but if your pages already have a lot
+  * of code that runs on page load, this may interfere 
+  * with it, so may have to be customized and integrated
+  * with your existing code.
+  */
+
+ /** WARNING:
+   * This lib has "use strict" defined. You may
+   * need to remove that if you are mixing this
+   * code with non-strict JavaScript.
+   */
+
+/* jshint strict:false */
+/* jshint esversion: 6*/
+/* jshint strict: global*/
+/* jshint browser: true */
+
+"use strict";
+
